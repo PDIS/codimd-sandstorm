3119dfd3c11662acfc6b6b2069eebe8f072b3467
Erickson Zhuang <bingluen0224@gmail.com>
2019-06-20 14:17:00 +0800

    Add sandstrom config

---
+++ .sandstorm/launcher.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Create a bunch of folders under the clean /var that php, nginx, and mysql expect to exist
+mkdir -p /var/lib/mysql
+mkdir -p /var/lib/mysql-files
+mkdir -p /var/log
+mkdir -p /var/log/mysql
+# Wipe /var/run, since pidfiles and socket files from previous launches should go away
+# TODO someday: I'd prefer a tmpfs for these.
+rm -rf /var/run
+rm -rf /var/tmp
+mkdir -p /var/tmp
+mkdir -p /var/run/mysqld
+
+# Ensure mysql tables created
+# HOME=/etc/mysql /usr/bin/mysql_install_db
+HOME=/etc/mysql /usr/sbin/mysqld --initialize
+
+# Spawn mysqld, php
+HOME=/etc/mysql /usr/sbin/mysqld --skip-grant-tables &
+# Wait until mysql and php have bound their sockets, indicating readiness
+while [ ! -e /var/run/mysqld/mysqld.sock ] ; do
+    echo "waiting for mysql to be available at /var/run/mysqld/mysqld.sock"
+    sleep .2
+done
+
+# Create database & user
+echo "CREATE DATABASE IF NOT EXISTS app; CREATE USER IF NOT EXISTS 'app'@'127.0.0.1' IDENTIFIED BY '20976d20aa2059a6e087773da9f41f07'; GRANT ALL ON `database`.* TO 'app'@'127.0.0.1'" | mysql -uroot
+
+cd /opt/app
+
+# Run migration
+sequelize db:migrate
+
+# Run app
+export NODE_ENV=production
+export CMD_ALLOW_ORIGIN=*.local.sandstorm.io
+export CMD_DOMAIN=$ROOT_URL
+export CMD_USECDN=false
+export CMD_LOG_LEVEL=debug
+export CMD_PROTOCOL_USESSL=false
+node app.js
---
+++ .sandstorm/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+
+# Install MySQL && NodeJS && yarn
+echo -e "deb http://repo.mysql.com/apt/debian/ stretch mysql-5.7\ndeb-src http://repo.mysql.com/apt/debian/ stretch mysql-5.7" > /etc/apt/sources.list.d/mysql.list
+wget -O /tmp/RPM-GPG-KEY-mysql https://repo.mysql.com/RPM-GPG-KEY-mysql
+apt-key add /tmp/RPM-GPG-KEY-mysql
+
+curl -sL https://deb.nodesource.com/setup_10.x | bash
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+apt-get update
+apt-get install -y mysql-server nodejs yarn git
+service mysql stop
+systemctl disable mysql
+# patch mysql conf to not change uid, and to use /var/tmp over /tmp
+# for secure-file-priv see https://github.com/sandstorm-io/vagrant-spk/issues/195
+sed --in-place='' \
+        --expression='s/^user\t\t= mysql/#user\t\t= mysql/' \
+        --expression='s,^tmpdir\t\t= /tmp,tmpdir\t\t= /var/tmp,' \
+        --expression='/\[mysqld]/ a\ secure-file-priv = ""\' \
+        /etc/mysql/my.cnf
+# patch mysql conf to use smaller transaction logs to save disk space
+cat <<EOF > /etc/mysql/conf.d/sandstorm.cnf
+[mysqld]
+# Set the transaction log file to the minimum allowed size to save disk space.
+innodb_log_file_size = 1048576
+# Set the main data file to grow by 1MB at a time, rather than 8MB at a time.
+innodb_autoextend_increment = 1
+EOF
+
+## NODE package
+npm install -g node-gyp sequelize-cli
+
